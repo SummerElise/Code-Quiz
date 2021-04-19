04c3898721b47bb19fa50515a89d895264d9e572
lissy <slissy513@gmail.com>
2021-04-19 01:07:12 -0400

    made another script file to hold my questions; changed my 'const' to 'var'; removed 'win' and 'lose' sections and made them 'correct' and 'wrong' sections instead; Working now on getting 'start' button to show questions when clicked

--- script.js
+++ script.js
@@ -1,183 +1,81 @@
-const start = document.getElementById("Start-Quiz");
+//Variables
+var win = document.querySelector(".win");
+var  = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var CorrectCounter = 0;
+var WrongCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
+var question = document.getElementById("question");
+var answer
 
-const quiz = document.getElementById("quiz");
-
-const question = document.getElementById("question");
-
-const qImg = document.getElementById("qImg");
-
-const choiceA = document.getElementById("A");
-
-const choiceB = document.getElementById("B");
-
-const choiceC = document.getElementById("C");
-
-const choiceC = document.getElementById("D");
-
-const choiceC = document.getElementById("E");
-
-const choiceC = document.getElementById("F");
-
-const choiceC = document.getElementById("G");
-
-const score = document.getElementById("score");
-
-
-
-let questions = [
-
-    {
-
-        question : "How many bones are in the human body?",
-
-        imgSrc : "",
-
-        choiceA : "166",
-
-        choiceB : "300",
-
-        choiceC : "206",
-
-        correct : "206"
-
-    },{
-
-        question : "What is the most abundant gas in the Earth's atmosphere?",
-
-        imgSrc : "",
-
-        choiceA : "Nitrogen",
-
-        choiceB : "Hydrogen",
-
-        choiceC : "Oxygen",
-
-        correct : "Nitrogen"
-
-    },{
-
-        question : "What is the biggest planet in our solar system?",
-
-        imgSrc : "img/js.png",
-
-        choiceA : "Saturn",
-
-        choiceB : "Mars",
-
-        choiceC : "Jupiter",
-
-        correct : "Jupiter"
-
-    },{
-
-        question : "What is a material that will not carry an electrical charge called?",
+// The init function is called when the page loads 
+function init() {
+    getCorrect();
+    getWrong();
+  }
+  
+  // The startGame function is called when the start button is clicked
+  function startQuiz() {    
+    titleScreen.setAttribute("class", "hide");    
+    quizScreen.setAttribute("class", "show");  
+    timerId = setInterval(tick, 1000);  
+    timeEl.textContent = time;  
+    getQuestion();
+  }
     
-        imgSrc : "",
-    
-        choiceA : "Conductor",
-    
-        choiceB : "Insulator",
-    
-        choiceC : "Unit",
-    
-        correct : "Insulator"
-    
-    },{
-        
-        question : "What scientist proposed the three laws of motion?",
-    
-        imgSrc : "",
-    
-        choiceA : "Newton",
-    
-        choiceB : "Aristotle",
-    
-        choiceC : "Rutherford",
-    
-        correct : "Newton"
-    
-    },{
-
-        
-        question : "If you were to be lowered into a well, your weight would:",
-    
-        imgSrc : "",
-    
-        choiceA : "increase slightly",
-    
-        choiceB : "decrease slightly",
-    
-        choiceC : "remain exactly the same",
-    
-        correct : "decrease slightly"
-    
-    },{
-
-        
-        question : "The study of how people use tools to perform work and how people physically relate to their working environment is called?",
-    
-        imgSrc : "",
-    
-        choiceA : "engineering",
-    
-        choiceB : "ergonomics",
-    
-        choiceC : "agronomy",
-    
-        correct : "ergonomics"
-    
-    }
-
-];
+  // The GameOver function is called when timer reaches 0
+  function GameOver() {
+    wordBlank.textContent = "GAME OVER";
+    loseCounter++
+    startButton.disabled = false;
+    setLosses()
+  }
+  
+  // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+  function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 1000);
+  }
 
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
-let count = 0;
-let TIMER;
-
 
 //Rendering a question
+startButton.addEventListener("click", startQuiz);
 
-function renderQuestion(){
-
-    let q = questions[runningQuestion];
-   
-
-    question.innerHTML = "<p>"+ q.question +"</p>";    
-    qImg.innerHTML = "<img src="+ q.imgSrc +">";
-
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-    choiceE.innerHTML = q.choiceE;
-    choiceF.innerHTML = q.choiceF;
-    choiceG.innerHTML = q.choiceG;
-}
-
-start.addEventListener("click", StartQuiz);
-
-//Start quiz
-function StartQuiz() {
-    start.style.display = "none";
+// start quiz
+const quiz = document.getElementById("quiz");
+function startQuiz(){
+    h2.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
-    TIMER = setInterval(renderCounter,)
-}
-
-start.style.display = "none"
-renderQuestion();
-quiz.style.display ="block";
-
-function renderCounter(){
-    if count
-}
+    renderCounter();
 
 //Check Answer
 var incorrect = [];
 
 function checkAnswer(answer){
-    if(answer == questions[runningQuestion].correct){
+    if(answer === questions[runningQuestion].correct){
         //if correct answer
         alert('Correct!');        
     } else {
@@ -186,13 +84,28 @@ function checkAnswer(answer){
     alert('Sorry, this is incorrect');
     }
 }   if(runningQuestion < lastQuestion){
-    runningQuestion++;
-    renderQuestion();
+    runningQuestion++;    
+}else{
+    clearInterval(TIMER);
+    scoreRender();
 }
 
-//Render Score
-function scoreRender(){
-    score.style.display = "block";
+// Attach event listener to start button to call startGame function on click
+startButton.addEventListener("click", startQuiz);
+
+// Calls init() so that it fires when page opened
+init();
+
+// Bonus: Add reset button
+var resetButton = document.querySelector(".reset-button");
+
+function resetGame() {
+  // Resets win and loss counts
+  winCounter = 0;
+  loseCounter = 0;
+  // Renders win and loss counts and sets them into client storage
+  setWins()
+  setLosses()
 }
-//Added EventListener
-start.addEventListener("click", StartQuiz);
+// Attaches event listener to button
+resetButton.addEventListener("click", resetGame);
