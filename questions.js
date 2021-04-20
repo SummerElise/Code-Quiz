6e4a92305e9b93a52dce3c0b6aac3f1c3334759a
lissy <slissy513@gmail.com>
2021-04-19 20:43:12 -0400

    Cleaned up my second script file; removed code in main script file in hopes of getting my questions to show when start button is clicked but to no avail; currently working in 'function: showQuestion' section of main script

--- questions.js
+++ questions.js
@@ -1,53 +1,54 @@
-var question
+var questions = document.getElementById("#questions");
  = [
     {
         question : "How many bones are in the human body?",
-        imgSrc : "",
         choiceA : "166",
         choiceB : "300",
-        choiceC : "206",
-        correct : "206"
-    },{
+        choiceC : "206"        
+    },
+        correct: "206"
+    {
         question : "What is the most abundant gas in the Earth's atmosphere?",
-        imgSrc : "",
         choiceA : "Nitrogen",
         choiceB : "Hydrogen",
-        choiceC : "Oxygen",
-        correct : "Nitrogen"
-    },{
+        choiceC : "Oxygen"        
+    },
+        correct: "Nitrogen"
+    {
         question : "What is the biggest planet in our solar system?",
-        imgSrc : "img/js.png",
         choiceA : "Saturn",
         choiceB : "Mars",
-        choiceC : "Jupiter",
+        choiceC : "Jupiter"
+    },
         correct : "Jupiter"
-    },{
+    {
         question : "What is a material that will not carry an electrical charge called?",    
-        imgSrc : "",    
         choiceA : "Conductor",    
         choiceB : "Insulator",    
-        choiceC : "Unit",    
-        correct : "Insulator"    
-    },{        
+        choiceC : "Unit"           
+    },
+         correct: "Insulator"
+    {        
         question : "What scientist proposed the three laws of motion?",    
-        imgSrc : "",    
         choiceA : "Newton",    
         choiceB : "Aristotle",    
-        choiceC : "Rutherford",    
-        correct : "Newton"    
-    },{        
+        choiceC : "Rutherford"    
+    },
+    
+        correct: "Newton"    
+    {        
         question : "If you were to be lowered into a well, your weight would:",    
-        imgSrc : "",    
         choiceA : "increase slightly",    
-        choiceB : "decrease slightly",    
-        choiceC : "remain exactly the same",    
-        correct : "decrease slightly"    
-    },{        
+        choiceB : "decrease slightly", 
+        choiceC: "remain exactly the same"  
+    },
+        correct: "decrease slightly"
+    {        
         question : "The study of how people use tools to perform work and how people physically relate to their working environment is called?",    
-        imgSrc : "",    
         choiceA : "engineering",    
         choiceB : "ergonomics",    
-        choiceC : "agronomy",    
-        correct : "ergonomics"    
+        choiceC : "agronomy"   
     }
+        correct: "ergonomics"    
+
 ];
--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 //Variables
-var win = document.querySelector(".win");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
-var CorrectCounter = 0;
-var WrongCounter = 0;
-var isWin = false;
+var timerElement = document.querySelector(".timer-counter");
+var startButton = document.querySelector("#start");
 var timer;
 var timerCount;
-var question = document.getElementById("question");
 var chosenAnswer ="";
+var score;
+var choices = document.querySelector("#choices");
+var checkAnswer = document.querySelector("check-Answer");
 
-// The init function is called when the page loads 
-function init() {
-    getCorrect();
-    getWrong();
+var questions = [];
+
+
+  function init() {
+    getScore();
   }
-  
+
   // The startGame function is called when the start button is clicked
-  function startQuiz() {    
-    isWin = false;
+  function startQuiz() {  
     timerCount = 60;
     startButton.disabled = true;
     renderQuestion()
@@ -26,11 +24,10 @@ function init() {
   }
     
   // The GameOver function is called when timer reaches 0
-  function GameOver() {
-    wordBlank.textContent = "GAME OVER";
-    loseCounter++
+  function QuizOver() {
+    wordBlank.textContent = "QUIZ RESULTS";    
     startButton.disabled = false;
-    setLosses()
+    setScore()
   }
   
   // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
@@ -41,44 +38,61 @@ function init() {
       timerElement.textContent = timerCount;
       if (timerCount >= 0) {
         // Tests if win condition is met
-        if (isWin && timerCount > 0) {
-          // Clears interval and stops timer
+        
           clearInterval(timer);
-          winGame();
+          setScore();
         }
-      }
+      
       // Tests if time has run out
       if (timerCount === 0) {
         // Clears interval
         clearInterval(timer);
-        loseGame();
+        setScore();
       }
     }, 1000);
   }
 
-  function renderQuestion() {
-      
+  function renderQuestions (){
+  var output = [];
+  var choices;    
+    for (var i=0; i <questions.length; i++) {
+      choices =[];
+    for(letter in questions[i].choices){
+      choices.push(
+        '<label>'
+        +'<input type="radio" name="questions'+i+'" value="'+letter+'">'
+        + letter + ': '
+        + questions[i].choices[letter]
+      + '</label>'
+    );
+  }
+  output.push(
+    '<div class="choices">' + questions[i].question + '</div>'
+    + '<div id=("A", "B", "C", "D", "E", "F", "G")> + .join('') + '</div>'
+  );
+}
+// finally combine our output list into one string of html and put it on the page
+quizContainer.innerHTML = output.join('');
+}
+  function setScore() {
+    score.textContent = scoreCounter;
+    localStorage.setItem("setScore", setScore)
   }
 
-//Check Answer
-var incorrect = [];
-
-function checkAnswer(answer){
-    if(answer === questions[runningQuestion].correct){
-        //if correct answer
-        alert('Correct!');        
+  function getScore() {
+    var storedScores = localStorage.getItem("scoreCounter");
+    if (storedScore === null) {
+      scoreCounter = 0;
     } else {
-        //if wrong answer      
-    incorrect.push(index);
-    alert('Sorry, this is incorrect');
+      scoreCounter = storedScore;
     }
-}   if(runningQuestion < lastQuestion){
-    runningQuestion++;    
-}else{
-    clearInterval(TIMER);
-    scoreRender();
-}
 
+    score.textContent = storedScore;
+  }
+
+  function checkAnswer(chosenChoice) {
+    var 
+  }
 // Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startQuiz);
 
@@ -90,11 +104,10 @@ var resetButton = document.querySelector(".reset-button");
 
 function resetGame() {
   // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
+  TIMER = 0;
+  l
   // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
+  
 }
 // Attaches event listener to button
 resetButton.addEventListener("click", resetGame);
