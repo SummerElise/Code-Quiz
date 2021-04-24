9b1d1df5dc8bb02a943ec17211c800bbcbe693c5
lissy <slissy513@gmail.com>
2021-04-23 21:54:21 -0400

    Having issues with my 'start' button not rendering questions therefore multiple deletions and additions to both html and js

--- script.js
+++ script.js
@@ -1,162 +1,153 @@
 //Variables
-var startButton = document.getElementById("startbtn");
+var startButton = document.getElementById("startButton");
 var quiz = document.getElementById("quiz");
 var questions = document.getElementById("questions");
-var quizTimer = document.getElementById("timer");
-var results = document.getElementById("result");
-var finalScore = document.getElementById("finalScore");
-var quizoverDiv = document.getElementById("Quizover");
-var scoreContainer = document.getElementById("scoreContainer");
-var highscoreDiv = document.getElementById("highscore-Page");
-var highscoreNameInput = document.getElementById("highscore-initials");
-var QuizOverBtns = document.getElementById("QuizOverBtns");
-var submitScoreBtn = document.getElementById(submitScore);
-var highscoreDisplayScore = document.getElementById("highscore-score");
 var buttonA = document.getElementById('A');
 var buttonB = document.getElementById('B');
 var buttonC = document.getElementById('C');
-var buttonD = document.getElementById('D');
-var buttonE = document.getElementById('E');
-var buttonF = document.getElementById('F');
-var buttonG = document.getElementById('G');
+var timer = document.getElementById("timer");
+var results = document.getElementById("result");
+var ViewHighscores = document.getElementById("ViewHighscores")
+var finalScore = document.getElementById("finalScore");
+var QuizoverDiv = document.getElementById("Quizover");
+var scoreContainer = document.getElementById("scoreContainer");
+var highscorePage = document.getElementById("highscore-Page");
+var NameInput = document.getElementById("initials");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscore = document.getElementById("highscore");
+var text = document.getElementById("text");
+
+
 
 //Questions with answers
-var quizQuestions = [
-  {
+var questions =
+  [{
       question : "How many bones are in the human body?",
       choiceA : "166",
       choiceB : "300",
       choiceC : "206",
-      correct= "206"},      
+      correctAnswer= "C"},      
   {    
       question: "What is the most abundant gas in the Earth's atmosphere?",
       choiceA : "Nitrogen",
       choiceB : "Hydrogen",
       choiceC : "Oxygen",
-      correct="Nitrogen"},  
+      correctAnswer="A"},  
   {
       question : "What is the biggest planet in our solar system?",
       choiceA : "Saturn",
       choiceB : "Mars",
       choiceC : "Jupiter",
-      correct= "Jupiter"},
+      correctAnswer= "C"},
   {
       question : "What is a material that will not carry an electrical charge called?",    
       choiceA : "Conductor",    
       choiceB : "Insulator",    
       choiceC : "Unit",
-      correct= "Insulator"},
+      correctAnswer= "B"},
   {        
       question : "What scientist proposed the three laws of motion?",    
       choiceA : "Newton",    
       choiceB : "Aristotle",    
       choiceC : "Rutherford",
-      correct="Newton"},  
+      correctAnswer="A"},  
   {        
       question : "If you were to be lowered into a well, your weight would:",    
       choiceA : "increase slightly",    
       choiceB : "decrease slightly", 
       choiceC: "remain exactly the same",
-      correct="decrease slightly"},
+      correctAnswer="B"},
   {        
       question : "The study of how people use tools to perform work and how people physically relate to their working environment is called?",    
       choiceA : "engineering",    
       choiceB : "ergonomics",    
       choiceC : "agronomy", 
-      correct="ergonomics"},
+      correctAnswer="B"}
 ];
 
-  var lastQuestionIndex = quizQuestions.length;
-  var currentQuestionIndex = 0;
-  var timeLeft = 60;
-  var timerInterval;
-  var score = 0;
-  var correct;
+  startButton.addEventListener("click",startQuiz)
+  ViewHighscores.addEventListener("click", displayHighscores)
 
-  // The startGame function is called when the start button is clicked; Timer begins; First quiz question appears.
-  function startQuiz() {  
-    startQuizDiv.style.display = "none";
-    quizoverDiv.style.display ="none";
-    generateQuizQuestion();
+   // The startGame function is called when the start button is clicked
+   function startQuiz() {  
+    ViewHighscores.style.display = "none"
+    finalScore.style.display = "none"
+    NameInput.style.display = "none"
+    showQuestions()
+    quiz.style.display = "block"
+    countdown()
+  }
+
+
+  function ViewHighscores(){
+    highscorePage.style.display = "block"
+    startButton.style.display = "none"
+    QuizoverDiv.style.display = "none";
+    scoreContainer.style.display = "flex";
+    highscorePage.style.display = "block";
+}
+  
+
+  function showQuestion(){
+    QuizoverDiv.style.display = "none";
+    if (currentQuestionIndex === lastQuestionIndex) {    
+      return showScore();
+    }
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;   
+  };
+
   
 
     //Timer function
     timerInterval = setInterval(function() {
       timeLeft--;
-      quizTimer.textContent = "Time left " + timeLeft;
+      quizTimer.textContent = "Time left: " + timeLeft;
         if(timeLeft === 0) {
           clearInterval(timerInterval);
           showScore();
         }
     }, 1000);
-    quiz
-  // The GameOver function is called when timer reaches 0
-  var timerCount = 60
-  // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-  function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // Tests if win condition is met
-        
-          clearInterval(timer);
-          setScore();
-        }
-      
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        setScore();
-      }
-    }, 1000);
-  }
-
-  function renderQuestions (){
-  var output = [];
-  var choices;    
-    for (var i=0; i <questions.length; i++) {
-      choices =[];
-    for(letter in questions[i].choices){
-      choices.push(
-        '<label>'
-        +'<input type="radio" name="questions'+i+'" value="'+letter+'">'
-        + letter + ': '
-        + questions[i].choices[letter]
-      + '</label>'
-    );
-  }
-  output.push(
+    quizBody.style.display = "block"
   
-  );
-}
-// finally combine our output list into one string of html and put it on the page
-quizContainer.innerHTML = output.join('');
 
 
-  function checkAnswer(chosenChoice) {
-    var 
+  // Screen at the end of the quiz that displays the score
+  function showScore() {
+  quizBody.style.display ="none";
+  QuizoverDiv.style.display = "flex";
+  clearInterval(timerInterval);
+  highscoreNameInput.value = "";
+  finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
   }
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startQuiz);
 
-// Calls init() so that it fires when page opened
-init();
+  submitScoreBtn.addEventListener("click", function highscore(){
 
-// Bonus: Add reset button
-refreshButtonEl.on('click', function () {
-  location.reload();
-});
+    if(highscoreNameInput.value === "") {
+      alert("Text box cannot be blank");
+      return false;
+    }else{
+      var savedHighscore = JSON.parse(localStorage.getItem("savedHighscore")) || [];
+      var currentUser = highscoreNameInput.value.trim();
+      var currentHighscore = {
+        name : currentUser,
+        score : score
+      };
+
+      quizoverDiv.style.display = "none";
+      scoreContainer.style.display = "flex";
+      highscoreDiv.style.display = "block";
+      QuizOverBtns.style.display ="flex";
+      savedHighscore.push(currentHighscore);
+      localStorage.setItem("savedHighscore", JSON.stringify(savedHighscore));
+      generateHighscore();
+
+    }
+  });
 
 
-function resetGame() {
-  // Resets win and loss counts
-  TIMER = 0;
-  l
-  // Renders win and loss counts and sets them into client storage
-  
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
+
+
