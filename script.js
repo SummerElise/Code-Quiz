ee608c21a03aa54938aa58dffc02707499abd0a2
lissy <slissy513@gmail.com>
2021-04-24 20:19:45 -0400

    Finally got my 'start' button functioning; currently working on getting my questions to load one at a time instead of all at once; changed my css file to accomodate new/removed elements

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ const questions = document.getElementById("questions");
 const choiceA = document.getElementById('A');
 const choiceB = document.getElementById('B');
 const choiceC = document.getElementById('C');
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
+const timer = document.getElementById("timer");
 const scoreDiv = document.getElementById("scoreContainer");
-
-
+const score = document.getElementById("score");
+const highscores = document.getElementById("highscores");
+const bestScore = document.getElementById("bestScore");
+const highscoreInitials = document.getElementById("highscoreInitials");
+const submitScore = document.getElementById("submitScore");
+const highscorePage = document.getElementById("highscorePage");
+const text = document.getElementById("text");
+const quizBody = document.getElementById("quizBody");
 
 
 //Questions with answers
@@ -19,115 +24,126 @@ let question =
       choiceA : "166",
       choiceB : "300",
       choiceC : "206",
-      correctAnswer= "C"
+      correct: "C"
     },      
   {    
       question: "What is the most abundant gas in the Earth's atmosphere?",
       choiceA : "Nitrogen",
       choiceB : "Hydrogen",
       choiceC : "Oxygen",
-      correctAnswer="A"
+      correct: "A"
     },  
   {
       question : "What is the biggest planet in our solar system?",
       choiceA : "Saturn",
       choiceB : "Mars",
       choiceC : "Jupiter",
-      correctAnswer= "C"
+      correct: "C"
     },
   {
       question : "What is a material that will not carry an electrical charge called?",    
       choiceA : "Conductor",    
       choiceB : "Insulator",    
       choiceC : "Unit",
-      correctAnswer= "B"
+      correct: "B"
     },
   {        
       question : "What scientist proposed the three laws of motion?",    
       choiceA : "Newton",    
       choiceB : "Aristotle",    
       choiceC : "Rutherford",
-      correctAnswer="A"
+      correct: "A"
     },  
   {        
       question : "If you were to be lowered into a well, your weight would:",    
       choiceA : "increase slightly",    
       choiceB : "decrease slightly", 
       choiceC: "remain exactly the same",
-      correctAnswer="B"
+      correct: "B"
     },
   {        
       question : "The study of how people use tools to perform work and how people physically relate to their working environment is called?",    
       choiceA : "engineering",    
       choiceB : "ergonomics",    
       choiceC : "agronomy", 
-      correctAnswer="B"
+      correct: "B"
     }
 ];
 
-  const lastQuestion = questions.length - 1;
-  let runningQuestion = 0;
-  let count = 0;
-  const timer = 60;
-  const gaugeWidth = 150;
-  const gaugeUnit = gaugeWidth / timer;
-  let TIMER;
-  let score = 0;
+start.addEventListener("click", startQuiz);
+highscores.addEventListener("click", ViewHighscores);
 
-  
-  function renderQuestion(){
-    let q = question[runningQuestion];
+ // The startQuiz function is called when the start button is clicked
+ function startQuiz() {  
+  start.style.display = "none"
+  highscores.style.display = "none"
+  bestScore.style.display = "none"
+  showQuestions;
+  quiz.style.display = "block"
+  countdown()
+}
 
-    questions.innerHTML = "<p>"+ q.question +"</p>";
+var counter = 60
+var interval  
+function countdown(){
+  interval = setInterval(function() {
+    counter--;
+    if (counter >= 0) {
+      span = document.getElementById("timer");
+      span.innerHTML = "Time: " + " " + counter;
+    }
+    if (counter === 0) {
+      alert( 'Quiz Over!');
+      clearInterval(counter);
+    }
+
+  }, 1000);
+};
+
+  var lastQuestionIndex = questions.length = 1
+  var runningQuestionIndex = 0
+
+  function showQuestions(){    
+
+    if(runningQuestionIndex > lastQuestionIndex){
+      quiz.style.display = "none"
+      timer.style.display = "none"
+      highscores.style.display = "block"
+      clearInterval(interval)
+      score.innerHTML = "Your score is " + "" + counter
+      submitScore.addEventListener("click", save)
+      function save(){
+        var users = JSON.parse(localStorage.getItem("userScores")) || []
+        users.push(counter)
+        localStorage.setItem("userScores", JSON.stringify(users))
+
+        var initials = document.getElementById("initials").value
+        var initials  = JSON.parse(localStorage.getItem("userInitials")) || []
+
+        initials.push(initials)
+        localStorage.setItem("userInitials", JSON.stringify(initials))
+
+        location.reload()
+      }
+    }else{
+    let q = questions[runningQuestionIndex];
+    questions.innerHTML = "<p>"+ q.questions +"</p>"
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
-
-  }
-
-  start.addEventListener("click", start);
-
-   // The startQuiz function is called when the start button is clicked
-   function start() {  
-    start.style.display = "none";
-    renderQuestion();
-    quiz.style.display = "block";
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000);
-  }
-
-
-  function renderCounter(){
-    if(count <= timer){
-      counter.innerHTMl = count;
-      timeGauge.style.width = count * gaugeUnit + "px";
-      count++
-    }else{
-      clearInterval(TIMER);
-      scoreRender();
-    }
-  }
-
-  function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
-      score++;    
-  }else{
-    score--;
-  }
-  count = 0;
-  if(runningQuestion < lastQuestion){
-    runningQuestion++;
-    renderQuestion();
-  }else{
-    clearInterval(TIMER);
-    scoreRender();
   }
 }
-
-function scoreRender(){
-  scoreDiv.style.display = "block";
-
-
+   
+  
+  function checkAnswer(answer){
+    if(questions[runningQuestionIndex].correct == answer){
+      runningQuestionIndex++
+      showQuestions()   
+  }else{
+    counter = counter - 10
+    runningQuestionIndex++
+    showQuestions()
+  }
 }
 
 
