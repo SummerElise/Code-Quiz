75427fad0b210dbc7d2bf34a01930a33de3d0567
lissy <slissy513@gmail.com>
2021-04-24 23:39:10 -0400

    rendered questions after a couple hrs of revising all files of code; corrected errors n console; trying to show form submission when Quiz is over

--- script.js
+++ script.js
@@ -1,152 +1,165 @@
 //Variables
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
-const questions = document.getElementById("questions");
-const choiceA = document.getElementById('A');
-const choiceB = document.getElementById('B');
-const choiceC = document.getElementById('C');
-const timer = document.getElementById("timer");
+const question = document.getElementById("question");
+const choices = document.getElementById("choices");
+const choice1 = document.getElementById('1');
+const choice2 = document.getElementById('2');
+const choice3 = document.getElementById('3');
 const scoreDiv = document.getElementById("scoreContainer");
 const score = document.getElementById("score");
-const highscores = document.getElementById("highscores");
+const highscorePage = document.getElementById("highscorePage");
 const bestScore = document.getElementById("bestScore");
 const highscoreInitials = document.getElementById("highscoreInitials");
 const submitScore = document.getElementById("submitScore");
-const highscorePage = document.getElementById("highscorePage");
 const text = document.getElementById("text");
 const quizBody = document.getElementById("quizBody");
 
 
 //Questions with answers
-let question =
+var questions =
   [{
       question : "How many bones are in the human body?",
-      choiceA : "166",
-      choiceB : "300",
-      choiceC : "206",
-      correct: "C"
+      choice1 : "166",
+      choice2 : "300",
+      choice3 : "206",
+      correct: "3"
     },      
   {    
       question: "What is the most abundant gas in the Earth's atmosphere?",
-      choiceA : "Nitrogen",
-      choiceB : "Hydrogen",
-      choiceC : "Oxygen",
-      correct: "A"
+      choice1 : "Nitrogen",
+      choice2 : "Hydrogen",
+      choice3 : "Oxygen",
+      correct: "1"
     },  
   {
       question : "What is the biggest planet in our solar system?",
-      choiceA : "Saturn",
-      choiceB : "Mars",
-      choiceC : "Jupiter",
-      correct: "C"
+      choice1 : "Saturn",
+      choice2 : "Mars",
+      choice3 : "Jupiter",
+      correct: "3"
     },
   {
       question : "What is a material that will not carry an electrical charge called?",    
-      choiceA : "Conductor",    
-      choiceB : "Insulator",    
-      choiceC : "Unit",
-      correct: "B"
+      choice1 : "Conductor",    
+      choice2 : "Insulator",    
+      choice3 : "Unit",
+      correct: "2"
     },
   {        
       question : "What scientist proposed the three laws of motion?",    
-      choiceA : "Newton",    
-      choiceB : "Aristotle",    
-      choiceC : "Rutherford",
-      correct: "A"
+      choice1 : "Newton",    
+      choice2 : "Aristotle",    
+      choice3 : "Rutherford",
+      correct: "1"
     },  
   {        
       question : "If you were to be lowered into a well, your weight would:",    
-      choiceA : "increase slightly",    
-      choiceB : "decrease slightly", 
-      choiceC: "remain exactly the same",
-      correct: "B"
+      choice1 : "increase slightly",    
+      choice2 : "decrease slightly", 
+      choice3: "remain exactly the same",
+      correct: "2"
     },
   {        
       question : "The study of how people use tools to perform work and how people physically relate to their working environment is called?",    
-      choiceA : "engineering",    
-      choiceB : "ergonomics",    
-      choiceC : "agronomy", 
-      correct: "B"
+      choice1 : "engineering",    
+      choice2 : "ergonomics",    
+      choice3 : "agronomy", 
+      correct: "2"
     }
 ];
 
 start.addEventListener("click", startQuiz);
-highscores.addEventListener("click", ViewHighscores);
+HS.addEventListener("click", HS);
+
+var counter = 60;
+var timer = document.getElementById("timer");
+var startScore = 0;
+
+timer.textContent = "Time: " + startScore;
 
  // The startQuiz function is called when the start button is clicked
  function startQuiz() {  
   start.style.display = "none"
-  highscores.style.display = "none"
-  bestScore.style.display = "none"
-  showQuestions;
+  HS.style.display = "none"
+  quizBody.style.display = "none"
   quiz.style.display = "block"
-  countdown()
-}
-
-var counter = 60
-var interval  
-function countdown(){
-  interval = setInterval(function() {
-    counter--;
-    if (counter >= 0) {
-      span = document.getElementById("timer");
-      span.innerHTML = "Time: " + " " + counter;
+  question.style.display = "block"
+  
+  
+ var timeInterval = setInterval(function() {
+  timer.textContent = "Time: " + counter;
+      counter-=1;
+    if (counter === 0 || questions.length === runningQuestionIndex+1) {
+      //not sure
+      scoreRender();
+     clearInterval(timeInterval);
+     timer.textContent = "Time:" + counter;
     }
-    if (counter === 0) {
-      alert( 'Quiz Over!');
-      clearInterval(counter);
-    }
-
   }, 1000);
+
+  showQuestions();
 };
 
-  var lastQuestionIndex = questions.length = 1
+  var lastQuestionIndex = questions.length -1;
   var runningQuestionIndex = 0
 
-  function showQuestions(){    
-
-    if(runningQuestionIndex > lastQuestionIndex){
-      quiz.style.display = "none"
-      timer.style.display = "none"
-      highscores.style.display = "block"
-      clearInterval(interval)
-      score.innerHTML = "Your score is " + "" + counter
-      submitScore.addEventListener("click", save)
-      function save(){
-        var users = JSON.parse(localStorage.getItem("userScores")) || []
-        users.push(counter)
-        localStorage.setItem("userScores", JSON.stringify(users))
-
-        var initials = document.getElementById("initials").value
-        var initials  = JSON.parse(localStorage.getItem("userInitials")) || []
-
-        initials.push(initials)
-        localStorage.setItem("userInitials", JSON.stringify(initials))
-
-        location.reload()
-      }
-    }else{
+  function showQuestions(){      
     let q = questions[runningQuestionIndex];
-    questions.innerHTML = "<p>"+ q.questions +"</p>"
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-  }
-}
+    question.innerHTML =  q.question;
+    choice1.innerHTML = q.choice1;
+    choice2.innerHTML = q.choice2;
+    choice3.innerHTML = q.choice3;
+  };
+
    
   
   function checkAnswer(answer){
     if(questions[runningQuestionIndex].correct == answer){
-      runningQuestionIndex++
-      showQuestions()   
-  }else{
-    counter = counter - 10
-    runningQuestionIndex++
-    showQuestions()
+      answerOutput.textContent = "Correct!"
+         
   }
-}
+  else{
+    answerOutput.textContent = "Wrong!"
+    counter -=10;
+  }
+  if (questions.length === runningQuestionIndex+1) {
+    scoreRender();
+    return;
+  }
+  runningQuestionIndex++;
+  showQuestions();
+};
 
+function scoreRender() {
+  score.style.display = "block";
+  quizBody.style.display = "none";
+  quiz.style.display = "none";
+
+  if (counter === 0 || questions.length -1) {
+    score.textContent = "Your score is " + counter + ".";
+  }
+};
 
   
+var endScore = {
+  userInfo: 'summer',
+  counter: '30'
+}
 
-    
+var endScore = [];
+
+var loadScores = function() {
+  var endScore = localStorage.getItem("userInfo, counter");
+
+  if (!endScore) {
+    return false;
+  }
+
+  for (var i = 0; i < endScore.length; i++) {
+    var endScore = endScore[i];
+    var listItemel = document.createElement('li');
+    listItemel.textContent = endScore;
+  }
+  endScore.push(endScore)
+};
